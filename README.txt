7e6265cddfe92d4a68f2f4eb2e2702e3e757e4d0
walas-01 <cuenta0danna@gmail.com>
2023-08-18 18:55:34 -0300

    Agregado README.TXT y ajustes menores

---
+++ README.txt
@@ -0,0 +1,6 @@
+This is a test to learn how to use FETCH API
+It is also has a mobile first design
+
+ToDo:
+-Make cards also have dark background
+-Fix buttons
--- toggler.js
+++ toggler.js
@@ -3,6 +3,7 @@ const body = document.querySelector("body")
 
 
 
+
 darkmodeToggler.addEventListener("click",(event) => {
     darkmodeToggler.classList.toggle("oscuro")
     body.classList.toggle("oscuro-body")
